dd790edd17ef6ddad9c6008a8b353085aa590f8a
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2025-01-23 23:10:04 +0100

    Add pacman functionality with creation and rendering logic

---
+++ pacman.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pacman->c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amhernandez <alejhern@student.42.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 02:16:47 by amhernandez       #+#    #+#             */
+/*   Updated: 2025/01/18 02:16:50 by amhernandez      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static void	free_pacman(t_pacman *pacman)
+{
+	int	index;
+
+	if (pacman->alive)
+	{
+		index = -1;
+		while (pacman->alive[++index])
+			mlx_delete_texture(pacman->alive[index]);
+		free(pacman->alive);
+	}
+	if (pacman->dead)
+	{
+		index = -1;
+		while (pacman->dead[++index])
+			mlx_delete_texture(pacman->dead[index]);
+		free(pacman->dead);
+	}
+	free(pacman);
+	pacman = NULL;
+}
+
+static void	render_pacman(t_game *game, t_pacman *pacman)
+{
+	mlx_texture_t	*texture;
+
+	texture = pacman->alive[0];
+	pacman->pos = render_object(game, pacman->key_in_map, texture);
+	if (!game->map[pacman->pos.y][pacman->pos.x].image)
+		free_pacman(pacman);
+}
+
+static void	new_pacman_constructor(t_pacman *pacman)
+{
+	pacman->key_in_map = 'P';
+	pacman->lives = 3;
+	pacman->speed = 1;
+	pacman->direction = 'R';
+	pacman->is_dead = false;
+	pacman->alive = NULL;
+	pacman->dead = NULL;
+}
+
+t_pacman	*create_pacman(t_game *game)
+{
+	t_pacman	*pacman;
+	int			fd;
+
+	pacman = malloc(sizeof(t_pacman));
+	if (!pacman)
+		return (NULL);
+	new_pacman_constructor(pacman);
+	fd = open("routes/pacman-alive.txt", O_RDONLY);
+	if (fd == -1)
+		return (free_pacman(pacman), NULL);
+	pacman->alive = get_sprites(fd, 3);
+	close(fd);
+	if (!pacman->alive)
+		return (free_pacman(pacman), NULL);
+	fd = open("routes/pacman-dead.txt", O_RDONLY);
+	if (fd == -1)
+		return (free_pacman(pacman), NULL);
+	pacman->dead = get_sprites(fd, 10);
+	close(fd);
+	if (!pacman->dead)
+		return (free_pacman(pacman), NULL);
+	render_pacman(game, pacman);
+	return (pacman);
+}
