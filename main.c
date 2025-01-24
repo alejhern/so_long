54c9f0b0b2e09144af28640510dad4f7832aca5c
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2025-01-24 20:10:05 +0100

    main en pruebas de desarrollo

--- main.c
+++ main.c
@@ -1,120 +1,73 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main2.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alejhern <alejhern@student.42barcelona.co  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/11 00:16:33 by alejhern          #+#    #+#             */
-/*   Updated: 2025/01/11 00:52:03 by alejhern         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include "libft/libft.h"
-#include <MLX42/MLX42.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "so_long.h"
 
-#define IMAGE_SIZE 128
-#define MOVE_SPEED 5
+// void key_handler(mlx_key_data_t keydata, void* param) {
+//     t_game* game = (t_game*)param;
 
-int					move_count = 0;
+//     if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+//         mlx_close_window(game->mlx);
 
-static mlx_image_t	*image;
+//     if (keydata.action == MLX_PRESS) {
+//         int new_x = game->player_x;
+//         int new_y = game->player_y;
 
-// Crear un color en formato RGBA
-int32_t	ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+//         if (keydata.key == MLX_KEY_W) new_y--;
+//         else if (keydata.key == MLX_KEY_S) new_y++;
+//         else if (keydata.key == MLX_KEY_A) new_x--;
+//         else if (keydata.key == MLX_KEY_D) new_x++;
+
+//         if (game->map[new_y][new_x] != '1') {
+//             game->player_x = new_x;
+//             game->player_y = new_y;
+//         }
+//     }
+// }
+
+void	init_game(t_game *game)
 {
-	return (r << 24 | g << 16 | b << 8 | a);
+	int	fd;
+    t_pacman *pacman;
+    t_ghost **ghost;
+
+	get_map(game, "sprites/map/map.ber");
+	fd = open("routes/pacman-base.txt", O_RDONLY);
+	if (fd == -1)
+		return ;
+	game->map_textures = get_sprites(fd, 3);
+	close(fd);
+	render_map(game);
+    pacman = create_pacman(game);
+    if (pacman == NULL)
+        return ;
+    ghost = create_ghosts(game, 4);
+    if (ghost == NULL)
+        return ;
 }
 
-// Llenar la imagen con colores aleatorios
-void	ft_randomize(void *param)
-{
-	(void)param;
-	for (uint32_t i = 0; i < image->width; ++i)
-	{
-		for (uint32_t y = 0; y < image->height; ++y)
-		{
-			uint32_t color = ft_pixel(rand() % 256, // R
-										rand() % 256, // G
-										rand() % 256, // B
-										255           // A (opaco)
-			);
-			mlx_put_pixel(image, i, y, color);
-		}
-	}
-}
+int main(void) {
+    t_game game;
+    // t_pacman pacman;
+    // t_ghost ghost;
 
-// Controlar el movimiento de la imagen con las teclas
-void	ft_hook(void *param)
-{
-	mlx_t	*mlx;
-	int		max_x;
-	int		max_y;
-	int		last_count;
+    game.mlx = mlx_init(800, 600, "Pac-Man", true);
+    if (!game.mlx) return EXIT_FAILURE;
 
-	mlx = param;
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	// Obtener los límites dinámicos de la ventana
-	max_x = mlx->width - IMAGE_SIZE;
-	max_y = mlx->height - IMAGE_SIZE;
-	last_count = move_count;
-	// Mover la imagen dentro de los límites dinámicos
-	if (mlx_is_key_down(mlx, MLX_KEY_UP) && image->instances[0].y > 0
-		&& ++move_count)
-		image->instances[0].y -= MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN) && image->instances[0].y < max_y
-		&& ++move_count)
-		image->instances[0].y += MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT) && image->instances[0].x > 0
-		&& ++move_count)
-		image->instances[0].x -= MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT) && image->instances[0].x < max_x
-		&& ++move_count)
-		image->instances[0].x += MOVE_SPEED;
-	if (last_count < move_count)
-		ft_printf_fd(STDOUT_FILENO, "Move count: %d\n", move_count);
-}
 
-int32_t	main(void)
-{
-	const int	width = 512;
-	const int	height = 512;
-	mlx_t		*mlx;
+    init_game(&game);
+    // pacman = create_pacman(&game);
+    // ghost = create_ghost(&game, 4);
+    
 
-	// Configura el tamaño inicial de la ventana
-	// Inicializar la ventana
-	mlx = mlx_init(width, height, "MLX42", true);
-	if (!mlx)
-	{
-		puts(mlx_strerror(mlx_errno));
-		return (EXIT_FAILURE);
-	}
-	// Crear una imagen
-	image = mlx_new_image(mlx, IMAGE_SIZE, IMAGE_SIZE);
-	if (!image)
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return (EXIT_FAILURE);
-	}
-	// Enviar la imagen a la ventana
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return (EXIT_FAILURE);
-	}
-	// Rellenar la imagen con colores aleatorios inicialmente
-	ft_randomize(NULL);
-	// Configurar hooks para actualizar la ventana y mover la imagen
-	mlx_loop_hook(mlx, ft_hook, mlx);
-	// Iniciar el bucle principal
-	mlx_loop(mlx);
-	// Terminar el programa
-	mlx_terminate(mlx);
-	return (EXIT_SUCCESS);
+    // update_tile_size(&game);
+    // render_map(&game);
+
+    // mlx_key_hook(game.mlx, key_handler, &game);
+    mlx_resize_hook(game.mlx, window_resize_handler, &game);
+    mlx_loop(game.mlx);
+
+    // Liberar recursos
+    clear_images(&game);
+
+    mlx_terminate(game.mlx);
+    return EXIT_SUCCESS;
 }
